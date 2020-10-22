68c09d1ca1bfcbc479a406b9b5155e846277ce0a
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2020-10-22 14:37:23 -0300

    Fixed indentation problems

--- main.rb
+++ main.rb
@@ -1,25 +1,24 @@
 def bubble_sort(arr)
-    count=0
-    while count < arr.length-1
-        if arr[count] > arr[count + 1]
-            arr[count], arr[count + 1] = arr[count + 1], arr[count]
-            count = 0
-        else
-            count += 1
-        end
+  count = 0
+  while count < arr.length - 1
+    if arr[count] > arr[count + 1]
+      arr[count], arr[count + 1] = arr[count + 1], arr[count]
+      count = 0
+    else
+      count += 1
     end
-    return arr
+  end
+  return arr
 end
 
 def bubble_sort_by(arr)
-    count = 0
-
-    while count < arr.length-1
-        if yield(arr[count], arr[count + 1]).positive?
-            arr[count], arr[count + 1] = arr[count + 1], arr[count]
-            count = 0
-        else
-            count += 1
-        end
+  count = 0
+  while count < arr.length - 1
+    if yield(arr[count], arr[count + 1]).positive?
+      arr[count], arr[count + 1] = arr[count + 1], arr[count]
+      count = 0
+    else
+      count += 1
     end
+  end
 end
